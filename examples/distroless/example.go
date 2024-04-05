3dcb6d8854c532ef85e1087b71cee6e13054434c
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-04-05 08:55:31 -0700

    docs: add distroless example
    This adds an example of using the following components with a vanilla OTel setup:
    
    - local visualization through the SpanLinkExporter + SimpleSpanProcessor
    - deterministic sample
    - baggage span processor
    
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

---
+++ examples/distroless/example.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/honeycombio/honeycomb-opentelemetry-go"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+// Implement an HTTP Handler function to be instrumented
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+}
+
+// Wrap the HTTP handler function with OTel HTTP instrumentation
+func wrapHandler() {
+	handler := http.HandlerFunc(httpHandler)
+	wrappedHandler := otelhttp.NewHandler(handler, "hello")
+	http.Handle("/hello", wrappedHandler)
+}
+
+// Running the example:
+// export OTEL_SERVICE_NAME=distroless-example
+// export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=XXXX"
+// export OTEL_EXPORTER_OTLP_ENDPOINT=https://api.honeycomb.io:443
+// export HONEYCOMB_ENABLE_LOCAL_VISUALIZATIONS=1
+// go run example.go
+func main() {
+	var opts []trace.TracerProviderOption
+	// Enable local visualizations
+	if enableLocalVisualizationsStr := os.Getenv("HONEYCOMB_ENABLE_LOCAL_VISUALIZATIONS"); enableLocalVisualizationsStr != "" {
+		var apikey, headers, serviceName string
+		if serviceName = os.Getenv("OTEL_SERVICE_NAME"); serviceName == "" {
+			serviceName = "unknown_service:go"
+		}
+
+		if headers = os.Getenv("OTEL_EXPORTER_OTLP_HEADERS"); headers != "" {
+			for _, header := range strings.Split(headers, ",") {
+				if strings.HasPrefix(header, "x-honeycomb-team=") {
+					if parts := strings.Split(header, "="); len(parts) > 1 {
+						apikey = parts[1]
+					}
+				}
+			}
+		}
+		enabled, _ := strconv.ParseBool(enableLocalVisualizationsStr)
+		if enabled {
+			exporter, _ := honeycomb.NewSpanLinkExporter(apikey, serviceName)
+			opts = append(opts, trace.WithSpanProcessor(trace.NewSimpleSpanProcessor(exporter)))
+		}
+	}
+	// Enable multi-span attributes
+	opts = append(opts, trace.WithSpanProcessor(honeycomb.NewBaggageSpanProcessor()))
+
+	if sampleRateStr := os.Getenv("SAMPLE_RATE"); sampleRateStr != "" {
+		sampleRate, err := strconv.Atoi(sampleRateStr)
+		if err == nil {
+			opts = append(opts, trace.WithSampler(honeycomb.NewDeterministicSampler(sampleRate)))
+		}
+	}
+
+	ctx := context.Background()
+	// Configure a new OTLP exporter using environment variables for sending data to Honeycomb over gRPC
+	client := otlptracegrpc.NewClient()
+	exp, err := otlptrace.New(ctx, client)
+	if err != nil {
+		log.Fatalf("failed to initialize exporter: %e", err)
+	}
+	opts = append(opts, trace.WithBatcher(exp))
+
+	// Create a new tracer provider with a batch span processor and the otlp exporter
+	tp := trace.NewTracerProvider(opts...)
+
+	// Handle shutdown to ensure all sub processes are closed correctly and telemetry is exported
+	defer func() {
+		_ = exp.Shutdown(ctx)
+		_ = tp.Shutdown(ctx)
+	}()
+
+	// Register the global Tracer provider
+	otel.SetTracerProvider(tp)
+
+	// Register the W3C trace context and baggage propagators so data is propagated across services/processes
+	otel.SetTextMapPropagator(
+		propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		),
+	)
+
+	// Initialize HTTP handler instrumentation
+	wrapHandler()
+	log.Fatal(http.ListenAndServe(":3030", nil))
+}
---
+++ examples/distroless/go.mod
@@ -0,0 +1,50 @@
+module github.com/honeycombio/honeycomb-opentelemetry-go/examples/distroless
+
+go 1.22.1
+
+require (
+	github.com/honeycombio/honeycomb-opentelemetry-go v0.10.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.50.0
+	go.opentelemetry.io/otel v1.25.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.25.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.25.0
+	go.opentelemetry.io/otel/sdk v1.25.0
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.0 // indirect
+	github.com/honeycombio/otel-config-go v1.13.1 // indirect
+	github.com/lufia/plan9stats v0.0.0-20231016141302-07b5767bb0ed // indirect
+	github.com/power-devops/perfstat v0.0.0-20221212215047-62379fc7944b // indirect
+	github.com/sethvargo/go-envconfig v0.9.0 // indirect
+	github.com/shirou/gopsutil/v3 v3.23.10 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	go.opentelemetry.io/contrib/instrumentation/host v0.46.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.46.1 // indirect
+	go.opentelemetry.io/contrib/propagators/b3 v1.21.1 // indirect
+	go.opentelemetry.io/contrib/propagators/ot v1.21.1 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.44.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v0.44.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.21.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.23.0 // indirect
+	go.opentelemetry.io/otel/metric v1.25.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.25.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.1.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
+	google.golang.org/grpc v1.63.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
