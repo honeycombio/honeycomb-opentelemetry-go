99534a376d0d89943f4e16640175af291d50c63c
Alex Boten <223565+codeboten@users.noreply.github.com>
2024-04-23 15:00:11 -0700

    deprecate: NewBaggageSpanProcessor replaced by go.opentelemetry.io/contrib/processors/baggage/baggagetrace.New
    Signed-off-by: Alex Boten <223565+codeboten@users.noreply.github.com>

--- baggage_span_processor.go
+++ baggage_span_processor.go
@@ -15,32 +15,16 @@
 package honeycomb
 
 import (
-	"context"
-
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/baggage"
+	"go.opentelemetry.io/contrib/processors/baggage/baggagetrace"
 	"go.opentelemetry.io/otel/sdk/trace"
 )
-
-type baggageSpanProcessor struct{}
-
-var _ trace.SpanProcessor = (*baggageSpanProcessor)(nil)
 
 // Returns a new baggageSpanProcessor.
 //
 // The Baggage span processor duplicates onto a span the attributes found
 // in Baggage in the parent context at the moment the span is started.
+//
+// Deprecated: Use go.opentelemetry.io/contrib/processors/baggage/baggagetrace.New instead
 func NewBaggageSpanProcessor() trace.SpanProcessor {
-	return &baggageSpanProcessor{}
+	return baggagetrace.New()
 }
-
-func (processor baggageSpanProcessor) OnStart(ctx context.Context, span trace.ReadWriteSpan) {
-	baggage := baggage.FromContext(ctx)
-	for _, entry := range baggage.Members() {
-		span.SetAttributes(attribute.String(entry.Key(), entry.Value()))
-	}
-}
-
-func (processor baggageSpanProcessor) OnEnd(s trace.ReadOnlySpan)       {}
-func (processor baggageSpanProcessor) Shutdown(context.Context) error   { return nil }
-func (processor baggageSpanProcessor) ForceFlush(context.Context) error { return nil }
